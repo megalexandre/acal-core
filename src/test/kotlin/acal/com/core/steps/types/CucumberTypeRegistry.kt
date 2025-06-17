19487030dbf3a09e227514d927bda23fb380bcfb
alexandre queiroz <alexandre.queiroz@syngenta.com>
2025-06-16 21:18:35 -0300

    customer

--- src/test/kotlin/acal/com/core/steps/types/CucumberTypeRegistry.kt
+++ src/test/kotlin/acal/com/core/steps/types/CucumberTypeRegistry.kt
@@ -1,6 +1,5 @@
 package acal.com.core.steps.types
 
-import acal.com.core.domain.entity.AuditInfo
 import acal.com.core.resouces.CustomerModel
 import io.cucumber.java.DataTableType
 import java.time.LocalDateTime
@@ -11,10 +10,8 @@ class CucumberTypeRegistry {
     fun customerModelTransformer(entry: Map<String, String>): CustomerModel {
         return CustomerModel(
             id = entry["id"] ?: "",
-            auditInfo = AuditInfo(
-                createdAt = LocalDateTime.now(),
-                updatedAt = LocalDateTime.now()
-            ),
+            createdAt = LocalDateTime.now(),
+            updatedAt = LocalDateTime.now(),
             name = entry["name"] ?: "",
             identityCard = entry["identity_card"] ?: "",
             phoneNumber = entry["phone_number"],
