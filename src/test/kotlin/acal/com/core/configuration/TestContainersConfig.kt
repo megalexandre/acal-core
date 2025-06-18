95c7a5b4af8062e2c9b5291a580829c42542d409
alexandre queiroz <megalexandre@gmail.com>
2025-06-18 08:47:21 -0300

    simplify

--- src/test/kotlin/acal/com/core/configuration/TestContainersConfig.kt
+++ src/test/kotlin/acal/com/core/configuration/TestContainersConfig.kt
@@ -1,20 +1,14 @@
 package acal.com.core.configuration
 
-import org.bson.Document
-import org.slf4j.LoggerFactory
+import com.mongodb.client.MongoClients
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import org.testcontainers.containers.MongoDBContainer
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
-import com.mongodb.client.MongoClients
-import org.springframework.boot.context.event.ApplicationReadyEvent
-import org.springframework.context.event.EventListener
+import org.testcontainers.containers.MongoDBContainer
 
 @TestConfiguration
 class TestContainersConfig {
-
-    private val logger = LoggerFactory.getLogger(TestContainersConfig::class.java)
 
     companion object {
         val mongoDBContainer = MongoDBContainer("mongo:8.0")
@@ -22,13 +16,10 @@ class TestContainersConfig {
     }
 
     @Bean
-    fun mongoTemplate(): MongoTemplate {
-        val connectionString = mongoDBContainer.getReplicaSetUrl("acal")
-        val mongoClientDatabaseFactory = SimpleMongoClientDatabaseFactory(
-            MongoClients.create(connectionString),
+    fun mongoTemplate(): MongoTemplate =
+        MongoTemplate(SimpleMongoClientDatabaseFactory(
+            MongoClients.create(mongoDBContainer.getReplicaSetUrl("acal")),
             "acal"
-        )
-        return MongoTemplate(mongoClientDatabaseFactory)
-    }
+        ))
 
 }
