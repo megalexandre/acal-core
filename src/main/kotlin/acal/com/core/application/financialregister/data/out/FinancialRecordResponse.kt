289c4670eca7bad7c36f46035af7cffb80bebe67
alexandre queiroz <megalexandre@gmail.com>
2025-09-04 21:56:15 -0300

    invoice qr code

--- src/main/kotlin/acal/com/core/application/financialregister/data/out/FinancialRecordResponse.kt
+++ src/main/kotlin/acal/com/core/application/financialregister/data/out/FinancialRecordResponse.kt
@@ -1,6 +1,6 @@
 package acal.com.core.application.financialregister.data.out
 
-import acal.com.core.domain.entity.*
+import acal.com.core.domain.entity.FinancialRecord
 import org.springframework.data.domain.Page
 import java.math.BigDecimal
 import java.time.LocalDateTime
@@ -11,14 +11,16 @@ class FinancialRecordResponse (
     val createdBy: String,
     val number: String,
     val total: BigDecimal,
-)
+    val reason: String
+    )
 
 fun FinancialRecord.toResponse(): FinancialRecordResponse = FinancialRecordResponse(
     id = id,
     createdAt = createdAt,
     createdBy = createdBy,
-    number = number,
-    total = total,
+    number = detail.number,
+    total = detail.total,
+    reason = detail.reason.name
 )
 
 fun Page<FinancialRecord>.toPage(): Page<FinancialRecordResponse> = this.map { it.toResponse() }
